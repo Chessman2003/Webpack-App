7772f15a276bc3f7670e26703b9b50ce9d4cc8db
DESKTOP-NG0TREK\user <pavlusha2003769@yandex.ru>
2023-11-07 16:13:56 +0300

    переработка элементов списка и добавление стилей для функции клика

--- src/List.jsx
+++ src/List.jsx
@@ -1,30 +1,49 @@
 import React, {Compoment} from "react";
 import ListItem from "./ListItem";
+import './List.scss'
 
 class List extends React.Component {
-    render() {
-        const items = [{
-            header: `Заголовок 1`,
-            text: `пункт 1`
-        }, {
-            header: `Заголовок 2`,
-            text: `пункт 2`
-        }, {
-            header: `Заголовок 3`,
-            text: `пункт 3`
-        }];
-
-        return(
-            <ul>
-                {items.map((item, index) => (
-                    <ListItem 
-                        key={index} 
-                        {...item}
-                    />
-                ))}
-            </ul>
-        );
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedItem: null
+        };
     }
-};
 
+    handleItemClick(item) {
+        this.setState((prevState) => ({
+            selectedItem: prevState.selectedItem === item ? null : item
+        }));
+    }
+
+
+    render() {
+      const {selectedItem} = this.state;
+
+
+     return(
+        <div>
+          <h1>Список элементов</h1><ul>
+                <ListItem
+                    title="Заголовок 1"
+                    text="Текст 1"
+                    onItemClick={() => handleItemClick("item1")}
+                    selected={selectedItem === "item1"} />
+
+                <ListItem
+                    title="Заголовок 2"
+                    text="Текст 2"
+                    onItemClick={() => handleItemClick("item2")}
+                    selected={selectedItem === "item2"} />
+
+                <ListItem
+                    title="Заголовок 3"
+                    text="Текст 3"
+                    onItemClick={() => handleItemClick("item3")}
+                    selected={selectedItem === "item3"} />
+            </ul>
+        </div>
+     );
+    }
+}
 export default List
--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,16 +1,19 @@
 import React, {Component} from "react";
 
 class ListItem extends React.Component {
-    
     render() {
-        const {text, header} = this.props
-        return (
-            <li>
-                <h2>{header}</h2>
-                <p>{text}</p>
+        const {title, text, onItemClick, selected} = this.props;
+
+        return(
+            <li
+                onClick={onItemClick}
+                className={listItem ${selected ? "selected" : ""}}
+            >
+                <h2>title</h2>
+                <p>text</p>
             </li>
         );
+  
     }
-};
-
+}
 export default ListItem
